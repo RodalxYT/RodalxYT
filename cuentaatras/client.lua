5c922318662a927a86be759c91e3296d2bb8a92f
RodalxYT <92567445+RodalxYT@users.noreply.github.com>
2021-10-15 08:12:44 +0200

    Cuenta atras
    Con este script usando el comando /cuentaatras se iniciara una cuenta atras automatica de 10 segundos para las negociaciones de tu servidor

---
+++ cuentaatras/client.lua
@@ -0,0 +1,33 @@
+RegisterCommand("cuentaatras", function ()
+    TriggerEvent ("chatMessage", "Astro RP", {0, 255, 0}, "10")
+
+    Citizen.Wait( 1000 )
+    TriggerEvent ("chatMessage", "Astro RP", {0, 255, 0}, "9")
+
+    Citizen.Wait( 1000 )
+    TriggerEvent ("chatMessage", "Astro RP", {0, 255, 0}, "8")
+
+    Citizen.Wait( 1000 )
+    TriggerEvent ("chatMessage", "Astro RP", {0, 255, 0}, "7")
+
+    Citizen.Wait( 1000 )
+    TriggerEvent ("chatMessage", "Astro RP", {0, 255, 0}, "6")
+
+    Citizen.Wait( 1000 )
+    TriggerEvent ("chatMessage", "Astro RP", {0, 255, 0}, "5")
+
+    Citizen.Wait( 1000 )
+    TriggerEvent ("chatMessage", "Astro RP", {0, 255, 0}, "4")
+
+    Citizen.Wait( 1000 )
+    TriggerEvent ("chatMessage", "Astro RP", {0, 255, 0}, "3")
+
+    Citizen.Wait( 1000 )
+    TriggerEvent ("chatMessage", "Astro RP", {0, 255, 0}, "2")
+
+    Citizen.Wait( 1000 )
+    TriggerEvent ("chatMessage", "Astro RP", {0, 255, 0}, "1")
+
+    Citizen.Wait( 1000 )
+    TriggerEvent ("chatMessage", "Astro RP", {0, 255, 0}, "YAA!")
+end)
---
+++ cuentaatras/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "adamant"
+
+game "gta5"
+
+Autor "Rodalx_YT"
+
+description "cuentaatras"
+
+version "1.0.0"
+
+client_script {
+    "client.lua"
+}
